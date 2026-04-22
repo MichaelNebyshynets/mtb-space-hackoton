d264dc18d2a612eb75f0ec6ac0c9712454a8b207
Michael Nebyshynets <nebyshynets.m@gmail.com>
2026-04-22 19:46:32 +0300

    GOvno 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  base: './',   // ← ВОТ СЮДА
+  server: {
     allowedHosts: [
       '.ngrok-free.dev',  // разрешает все поддомены ngrok-free.dev
       'localhost'
